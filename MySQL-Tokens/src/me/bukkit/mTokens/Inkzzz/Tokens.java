f69b062e073416fa51dea0b0d9dc190935cdaa2c
Luke Denham <Luke Denham@192.168.1.75>
2015-05-28 02:44:20 +0100

    MySQL-Tokens - Project Start.

---
+++ MySQL-Tokens/src/me/bukkit/mTokens/Inkzzz/Tokens.java
@@ -0,0 +1,15 @@
+package me.bukkit.mTokens.Inkzzz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tokens extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
