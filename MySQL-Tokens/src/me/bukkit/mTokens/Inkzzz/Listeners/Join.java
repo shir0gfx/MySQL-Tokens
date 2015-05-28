153368ff906abe37ea1878f9cae41cec365f9b0e
Luke Denham <Luke Denham@192.168.1.75>
2015-05-28 04:02:53 +0100

    MySQL working - tested - commands to do.

--- MySQL-Tokens/src/me/bukkit/mTokens/Inkzzz/Listeners/Join.java
+++ MySQL-Tokens/src/me/bukkit/mTokens/Inkzzz/Listeners/Join.java
@@ -1,5 +1,6 @@
 package me.bukkit.mTokens.Inkzzz.Listeners;
 
+import me.bukkit.mTokens.Inkzzz.FetchData;
 import me.bukkit.mTokens.Inkzzz.SetData;
 
 import org.bukkit.entity.Player;
@@ -8,13 +9,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Join implements Listener {
-	
-	SetData sd = new SetData(); 
+
+	SetData sd = new SetData();
+	FetchData fd = new FetchData();
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
-		
+		if (!fd.isInDb(p)) {
+			sd.giveTokens(p, 0);
+		}
 	}
-	
+
 }
--- MySQL-Tokens/src/me/bukkit/mTokens/Inkzzz/Tokens.java
+++ MySQL-Tokens/src/me/bukkit/mTokens/Inkzzz/Tokens.java
@@ -1,17 +1,31 @@
 package me.bukkit.mTokens.Inkzzz;
 
+import me.bukkit.mTokens.Inkzzz.Listeners.Join;
+
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tokens extends JavaPlugin {
 
+	private MySQL mysql = new MySQL();
+	
 	private static Tokens instance;
 	
 	public void onEnable() {
 		instance = this;
-	}
+		loadListeners();
+		getConfig().options().copyDefaults(true);
+		saveDefaultConfig();
+		mysql.checkTable();
+	} 
 	
 	public void onDisable() {
 		instance = null;
+	}
+	
+	private void loadListeners() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new Join(), this);
 	}
 	
 	public static Tokens getInstance() { return instance; }
